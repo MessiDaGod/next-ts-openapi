ff995e024935ce90bafb0ff4ba6847f6c23f95e0
joeshakely <joeshakely@gmail.com>
2023-02-15 15:02:47 -0800

    good

--- src/pages/api/dataGridResize.ts
+++ src/pages/api/dataGridResize.ts
@@ -14,48 +14,129 @@ String.prototype.visualLength = function () {
     return 0;
   };
 
+  function $(id: Element) {
+    return (id ?? new Element) as HTMLElement;
+  }
+
+  function setListeners(div: HTMLElement): void {
+    var pageX: number | undefined,
+      curCol: HTMLElement | null,
+      nxtCol: HTMLElement | null,
+      prevCol: HTMLElement | null,
+      curColWidth: number | undefined,
+      nxtColWidth: number | undefined,
+      // @ts-ignore
+      prevColWidth: number | undefined;
+    ``;
+
+    if (div.parentElement) {
+      div.parentElement.addEventListener("mouseenter", function (e: MouseEvent): void {
+
+        let dataColumnId = (e.target as HTMLElement).dataset.columnId;
+        let tbl = this.closest("table") as HTMLTableElement;
+        if (tbl) {
+          let allCells = Array.from(new Set([...tbl.querySelectorAll('[data-column-id="' + dataColumnId + '"]')]));
+          if (allCells)
+            allCells.forEach((cell) => {
+              (cell as HTMLElement).style.borderRight = "4px solid #0000ff";
+            });
+        }
+      });
+
+      div.parentElement.addEventListener("mouseout", function (e: MouseEvent): void {
+        let dataColumnId = (e.target as HTMLElement).dataset.columnId;
+        let tbl = this.closest("table") as HTMLTableElement;
+        if (tbl) {
+          let allCells = Array.from(new Set([...tbl.querySelectorAll('[data-column-id="' + dataColumnId + '"]')]));
+          if (allCells)
+            allCells.forEach((cell) => {
+              (cell as HTMLElement).style.borderRight = "";
+            });
+        }
+      });
+
+
+      div.parentElement.addEventListener("dblclick", function (_e: MouseEvent): void {
+      var tbl = this.closest("table") as HTMLTableElement;
+      var columns = Array.from(new Set([...tbl.querySelectorAll("th")]));
+      columns.forEach((th) => {
+        if (th.textContent)
+          th.style.width = th.textContent.visualLength() + 13 + "px";
+      });
+    });
+
+    div.parentElement.addEventListener("mousedown", function (e: MouseEvent): void {
+      var target = e.target as HTMLElement;
+      curCol = target ? target : null;
+      var nextCol = curCol
+        ? (curCol.nextElementSibling as HTMLElement)
+        : null;
+      nextCol = nextCol
+        ? (nextCol?.nextElementSibling as HTMLElement)
+        : null;
+      if (curCol)
+        prevCol = curCol
+          ? (curCol.previousElementSibling as HTMLElement)
+          : null;
+      pageX = e.pageX;
+
+      const padding = curCol ? paddingDiff(curCol) : 0;
+
+      curColWidth =
+        curCol && curCol.offsetWidth > 0 && curCol.offsetWidth > padding
+          ? curCol.offsetWidth - padding
+          : 0;
+      if (nxtCol) nxtColWidth = nxtCol.offsetWidth - padding;
+
+      if (prevCol) prevColWidth = prevCol.offsetWidth - padding;
+    });
+
+    document.addEventListener("mousemove", function (e: MouseEvent): void {
+      if (curCol) {
+        const diffX = e.pageX - (pageX ?? 0);
+
+        if (nxtCol) {
+          nxtCol.style.minWidth = (nxtColWidth ?? 0) - diffX + "px";
+          nxtCol.style.width = (nxtColWidth ?? 0) - diffX + "px";
+        }
+        curCol.style.minWidth = (curColWidth ?? 0) + diffX + "px";
+        curCol.style.width = (curColWidth ?? 0) + diffX + "px";
+      }
+    });
+
+    document.addEventListener("mouseup", function (_e: MouseEvent): void {
+      curCol = null;
+      nxtCol = null;
+      pageX = undefined;
+      nxtColWidth = undefined;
+      curColWidth = undefined;
+    });
+  }
+}
+
+  function paddingDiff(col: HTMLElement): number {
+    if (getStyleVal(col, "box-sizing") === "border-box") {
+      return 0;
+    }
+    const padLeft = getStyleVal(col, "padding-left");
+    const padRight = getStyleVal(col, "padding-right");
+    return parseInt(padLeft) + parseInt(padRight);
+  }
+
+  function getStyleVal(elm: HTMLElement, css: string): string {
+    return window.getComputedStyle(elm, null).getPropertyValue(css);
+  }
+
 export const dataGridResize = () => {
 
   initResizeListeners();
-  if (!document) return;
-  var tables = Array.from(new Set([...document.querySelectorAll('table[id^="' + "gridjs_" + '"]')]));
-
-  tables.forEach((table) => {
-    if (window.location.href.includes("ssms")) {
-      const cells = Array.from(new Set([...table.querySelectorAll("td")]));
-      let pay = document.getElementById("payableStatus") as HTMLElement;
-      let istatus: number;
-      // istatus = !pay.value ? -1 : pay.value;
-      istatus =
-        pay !== null && pay.textContent ? parseInt(pay.textContent, 10) : -1;
-
-      cells.forEach((cell) => {
-        let attr = cell.getAttribute("data-column-id");
-        if (attr && attr === "STATUS") {
-          let span = cell.querySelector("span");
-          if (span && span.classList.length > 0) {
-            let status = "";
-            switch (istatus) {
-              case -1:
-                status = "none";
-                break;
-              case 0:
-                status = "fail";
-                break;
-              case 1:
-              case 2:
-                status = "success";
-                break;
-              default:
-                status = "none";
-                break;
-            }
-            span.classList.add(status);
-          }
-        }
-      });
+  let resizeDivs = Array.from(new Set([...document.querySelectorAll('div[class*="' + "columndivider" + '"]')]));
+  if (resizeDivs && resizeDivs.length > 0)
+  {
+    for (let i = 0; i < resizeDivs.length; i++) {
+      setListeners(resizeDivs[i] as HTMLDivElement);
     }
-  });
+  }
 
   function initResizeListeners() {
     const tables = [
--- src/pages/dataGrid.tsx
+++ src/pages/dataGrid.tsx
@@ -88,94 +88,6 @@ function handleSetData() {
   dataGridResize();
 }
 
-function setListeners(div: HTMLElement): void {
-  var pageX: number | undefined,
-    curCol: HTMLElement | null,
-    nxtCol: HTMLElement | null,
-    prevCol: HTMLElement | null,
-    curColWidth: number | undefined,
-    nxtColWidth: number | undefined,
-    // @ts-ignore
-    prevColWidth: number | undefined;
-  ``;
-  div.addEventListener("dblclick", function (_e: MouseEvent): void {
-    var tbl = this.closest("table") as HTMLTableElement;
-    var columns = Array.from(new Set([...tbl.querySelectorAll("th")]));
-    columns.forEach((th) => {
-      if (th.textContent)
-        th.style.width = th.textContent.visualLength() + 13 + "px";
-    });
-  });
-
-  div.addEventListener("mousedown", function (e: MouseEvent): void {
-    var target = e.target as HTMLElement;
-    curCol = target ? target.parentElement : null;
-    var nextCol = curCol
-      ? (curCol.nextElementSibling as HTMLElement)
-      : null;
-    nextCol = nextCol
-      ? (nextCol?.nextElementSibling as HTMLElement)
-      : null;
-    if (curCol)
-      prevCol = curCol
-        ? (curCol.previousElementSibling as HTMLElement)
-        : null;
-    pageX = e.pageX;
-
-    const padding = curCol ? paddingDiff(curCol) : 0;
-
-    curColWidth =
-      curCol && curCol.offsetWidth > 0 && curCol.offsetWidth > padding
-        ? curCol.offsetWidth - padding
-        : 0;
-    if (nxtCol) nxtColWidth = nxtCol.offsetWidth - padding;
-
-    if (prevCol) prevColWidth = prevCol.offsetWidth - padding;
-  });
-
-  div.addEventListener("mouseover", function (e: MouseEvent): void {
-    (e.target as HTMLElement).style.borderRight = "2px solid #0000ff";
-  });
-
-  div.addEventListener("mouseout", function (e: MouseEvent): void {
-    (e.target as HTMLElement).style.borderRight = "";
-  });
-
-  document.addEventListener("mousemove", function (e: MouseEvent): void {
-    if (curCol) {
-      const diffX = e.pageX - (pageX ?? 0);
-
-      if (nxtCol) {
-        nxtCol.style.minWidth = (nxtColWidth ?? 0) - diffX + "px";
-        nxtCol.style.width = (nxtColWidth ?? 0) - diffX + "px";
-      }
-      curCol.style.minWidth = (curColWidth ?? 0) + diffX + "px";
-      curCol.style.width = (curColWidth ?? 0) + diffX + "px";
-    }
-  });
-
-  document.addEventListener("mouseup", function (_e: MouseEvent): void {
-    curCol = null;
-    nxtCol = null;
-    pageX = undefined;
-    nxtColWidth = undefined;
-    curColWidth = undefined;
-  });
-}
-
-function paddingDiff(col: HTMLElement): number {
-  if (getStyleVal(col, "box-sizing") === "border-box") {
-    return 0;
-  }
-  const padLeft = getStyleVal(col, "padding-left");
-  const padRight = getStyleVal(col, "padding-right");
-  return parseInt(padLeft) + parseInt(padRight);
-}
-
-function getStyleVal(elm: HTMLElement, css: string): string {
-  return window.getComputedStyle(elm, null).getPropertyValue(css);
-}
-
 export function DataGrid() {
   // @ts-ignore
   const [data, setData] = React.useState(() => [...defaultData]);
@@ -194,30 +106,8 @@ export function DataGrid() {
       getCoreRowModel: getCoreRowModel(),
     }
   );
-  // interface Column {
-  //   Header: string;
-  //   accessor: string;
-  //   width?: number;
-  // }
 
-  // interface Data {
-  //   [key: string]: any;
-  // }
-
-  // interface Props {
-  //   columns: Column[];
-  //   data: Data[];
-  // }
-
-  function highlightColumnDivider(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    const target = event.target as HTMLElement;
-    target.classList.add(styles["hover"]);
-  }
-
-  function unhighlightColumnDivider(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    const target = event.target as HTMLElement;
-    target.classList.remove(styles["hover"]);
-  }
+  const propertyNames = Object.keys(data[0]);
 
   return (
     <div className={styles["dataGridhtml"]}>
@@ -227,7 +117,7 @@ export function DataGrid() {
           {table.getHeaderGroups().map((headerGroup, value) => (
             <tr key={`${headerGroup.id}${value}`}>
               {headerGroup.headers.map((header, value: number) => (
-                <th key={`${header.id}${value}`} className={styles["dataGridth"]} data-column-id={columns[value].header} scope="col" onMouseEnter={highlightColumnDivider} onMouseLeave={unhighlightColumnDivider} {...setListeners}>
+                <th key={`${header.id}${value}`} className={styles["dataGridth"]} data-column-id={columns[value].header} scope="col">
                   {header.isPlaceholder
                     ? null
                     : flexRender(
@@ -240,10 +130,10 @@ export function DataGrid() {
           ))}
         </thead>
         <tbody className={styles["dataGridtbody"]}>
-          {table.getRowModel().rows.map((row, value: number) => (
-            <tr id={"tr" + value.toString()} key={`${row.id}${value}`} className="gridjs-tr">
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} className={styles["dataGridtd"]}>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={`${row.id}`} className="gridjs-tr">
+              {row.getVisibleCells().map((cell, value: number) => (
+                <td key={cell.id} className={styles["dataGridtd"]} data-column-id={columns[value].header}>
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
@@ -254,7 +144,7 @@ export function DataGrid() {
           {table.getFooterGroups().map((footerGroup, value: number) => (
             <tr key={`${footerGroup.id}${value}`}>
               {footerGroup.headers.map((header) => (
-                <th key={`${header.id}${value}`}>
+                <th key={`${header.id}${value}`} data-column-id={columns[value].header}>
                   {header.isPlaceholder
                     ? null
                     : flexRender(
