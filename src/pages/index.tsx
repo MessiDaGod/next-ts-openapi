d5239e1227741593bda8bbc43ce9b431bdfcfd26
joeshakely <joeshakely@gmail.com>
2023-02-18 09:36:15 -0800

    sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import styles from "@/styles/Home.module.css";
 import { DataGrid } from "./dataGrid";
+import Sidebar from "./sidebar";
 
 export default function Home() {
   return (
     <div>
       <main className={styles.main}>
-        {DataGrid()}
+      <Sidebar />
+      {DataGrid()}
       </main>
     </div>
   );
---
+++ src/pages/sidebar.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import styles from "./sidebar.module.css";
+
+interface SidebarItem {
+  Name: string;
+  Path: string;
+  Icon: string;
+  New?: boolean;
+  Updated?: boolean;
+}
+
+interface Menu {
+  topBar: { id: string; label: string; url: string }[];
+  sideMenu: { id: string; label: string; url: string }[];
+  sidebar: SidebarItem[];
+}
+
+function Sidebar() {
+  const [sidebarItems, setSidebarItems] = useState<SidebarItem[]>([]);
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  useEffect(() => {
+    async function fetchMenu() {
+      const response = await fetch("/menu.json");
+      const data = await response.json();
+      setSidebarItems(data.sidebar);
+    }
+
+    fetchMenu();
+  }, []);
+
+  // Render the sidebar items
+  return (
+    <div
+      className={`${styles.sidebar} ${
+        isExpanded ? styles.expanded : styles.collapsed
+      }`}
+    >
+      <button
+        className={styles.expandButton}
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <span className={`material-symbols-outlined ${styles.expandIcon}`}>menu</span>
+      </button>
+      <nav>
+        <ul>
+          {sidebarItems.map((item) => (
+            <li key={item.Path} className={styles["li"]}>
+              <a href={item.Path}>
+                <span className={`material-symbols-outlined ${styles.icon}`}>
+                  {item.Icon}
+                </span>
+                {item.Name}
+                {item.New && <span>New</span>}
+                {item.Updated && <span>Updated</span>}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Sidebar;
