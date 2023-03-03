f53663fa585d158347fd8c59e6ffa50de8cf04be
joeshakely <joeshakely@gmail.com>
2023-03-03 14:50:30 -0800

    asdf

--- src/pages/pagination.tsx
+++ src/pages/pagination.tsx
@@ -82,7 +82,7 @@ export function Pagination({
           (pageNumber) =>
             pageNumber === 1 && (
               <p key={currentPage}>
-                Page <br />{currentPage}of {totalPages}&nbsp;
+                Page <br />{currentPage}of&nbsp;{totalPages}&nbsp;
               </p>
             )
         )}
