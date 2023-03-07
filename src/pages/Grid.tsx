79123e0ff2c0360af9f5dac14e133791de4790c9
joeshakely <joeshakely@gmail.com>
2023-03-06 18:22:18 -0800

    casd

--- src/pages/Grid.tsx
+++ src/pages/Grid.tsx
@@ -1,26 +1,24 @@
 import { useState } from "react";
 import cn from "classnames";
-import styles from "./Home.module.scss";
-import stylesWithin from "./GenericDropdown.module.scss";
+import gridStyles from "./Grid.module.scss";
+import genericStyles from "./GenericDropdown.module.scss";
+import dynamicStyles from "./DynamicGrid.module.scss";
 import React from "react";
 import Dropdown from "./dropdown";
 import DynamicGrid from "./DynamicGrid";
-// import DataGridDropdown from "./DataGridDropdown";
-// import GenericDropdown from "./GenericDropdown";
-import { Log, isColumnHidden } from "./utils";
-// import MultiDropdown from "./MultiDropdown";
+import DataGridDropdown from "./DataGridDropdown";
+import GenericDropdown from "./GenericDropdown";
 // import GenericDropdown from "./GenericDropdown";
 // import GoodColumns from "../../public/GoodColumns.json";
 // import dimensions from "../../public/Dimensions.json";
 // import DynamicGridProps from "./DynamicGrid";
-import properties from "../../public/propOptions.json";
-import { TableHeader } from "./TableHeader";
-import GenericDropdown from "./GenericDropdown";
 
 export default function Grid({
   tableRef,
+  columns,
 }: {
-  tableRef?: React.RefObject<HTMLDivElement>;
+  tableRef: React.RefObject<HTMLDivElement>;
+  columns: string[];
 }) {
   const [error, setError] = useState(null);
   const [status, setStatus] = useState("typing");
@@ -44,7 +42,6 @@ export default function Grid({
   React.useEffect(() => {
     console.info("Grid useEffect ran");
     setNumItems(1);
-    handleOnClick();
   }, []);
 
   async function handleTextareaChange(e) {
@@ -60,48 +57,29 @@ export default function Grid({
     }
   }
 
-  function handleOnClick() {
-    Log("click handled!!!!!!");
-    const elementsWithZIndex = document.querySelectorAll('[style*="z-index"]');
-    elementsWithZIndex.forEach((element) => {
-      if ((element as HTMLElement).id !== "table")
-        (element as HTMLElement).style.zIndex = "0";
-    });
-  }
-  const data = properties;
-  const columns = Object.keys(data[0]);
-  const header = columns.map((cols, idx: number) => {
-    return (
-      !isColumnHidden(data, cols) && (
-        <TableHeader key={cols} columnName={cols}>
-          <div className={styles["coldivider"]}></div>
-        </TableHeader>
-      )
-    );
-  });
-
   return (
-    <div className={styles["container"]}>
-      <div className={stylesWithin["tr"]} data-row-id="0">
-        <div className={stylesWithin["td"]} data-column-id="DROPDOWN">
-          <Dropdown
-            jsonFileName="GetOptions"
-            label="Choose Item to Display"
-            onItemChange={(e) => handleSetItem(e)}
-            showCheckbox={true}
-          />
-        </div>
-        <div className={stylesWithin["td"]}>
-          <GenericDropdown
-            selectItem={"GetPropOptions"}
-            showPagination={true}
-            showCheckbox={false}
-            columns={columns["Property"]}
-          />
-        </div>
+    <>
+      <div className={gridStyles["container"]}>
+        <Dropdown
+          className={gridStyles["dynamicgrid-dd"]}
+          jsonFileName="GetOptions"
+          label="Choose Item"
+          onItemChange={(e) => handleSetItem(e)}
+          showCheckbox={true}
+        />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <GenericDropdown
+          className={gridStyles["dynamicgrid-dd"]}
+          selectItem={"GetPropOptions"}
+          showPagination={true}
+          showCheckbox={false}
+        />
+      </div>
+      <div className={gridStyles["container"]}>
         <form>
           <input
-            className={cn(styles["rz-textbox"], styles["input"])}
+            className={cn(gridStyles["rz-textbox"], gridStyles["input"])}
             type="number"
             value={numItems}
             onChange={handleTextareaChange}
@@ -110,16 +88,17 @@ export default function Grid({
           <br />
           {error !== null && <p className="Error">{error.message}</p>}
         </form>
+        {status === "success" && (
+          <DynamicGrid
+            className={dynamicStyles["dynamicgrid-dd"]}
+            key={item}
+            selectItem={item}
+            showPagination={true}
+            numItems={numItems}
+          />
+        )}
       </div>
-      {status === "success" && (
-        <DynamicGrid
-          key={item}
-          selectItem={item}
-          showPagination={true}
-          numItems={numItems}
-        />
-      )}
-    </div>
+    </>
   );
 }
 
